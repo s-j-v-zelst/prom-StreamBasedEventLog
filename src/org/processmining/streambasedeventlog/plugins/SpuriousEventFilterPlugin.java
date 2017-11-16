8dcd10ea3bc2c9358b352550af6515313343f13d
svzelst <s.j.v.zelst@gmail.com>
2017-11-16 11:51:35 +0100

    disable deprecated plugin in ProM

--- src/org/processmining/streambasedeventlog/plugins/SpuriousEventFilterPlugin.java
+++ src/org/processmining/streambasedeventlog/plugins/SpuriousEventFilterPlugin.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import org.deckfour.uitopia.api.event.TaskListener.InteractionResult;
 import org.processmining.contexts.uitopia.UIPluginContext;
-import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
 import org.processmining.eventstream.connections.XSEventXSAuthorXSStreamConnectionImpl;
 import org.processmining.eventstream.core.factories.XSEventStreamFactory;
 import org.processmining.eventstream.core.interfaces.XSEventStream;
@@ -15,22 +14,19 @@ import org.processmining.eventstream.models.XSEventHub;
 import org.processmining.framework.connections.Connection;
 import org.processmining.framework.connections.ConnectionCannotBeObtained;
 import org.processmining.framework.plugin.PluginContext;
-import org.processmining.framework.plugin.annotations.Plugin;
-import org.processmining.framework.plugin.annotations.PluginVariant;
 import org.processmining.stream.connections.XSAuthorXSStreamConnectionImpl;
 import org.processmining.stream.core.enums.CommunicationType;
 import org.processmining.streambasedeventlog.algorithms.ConditionalProbabilitiesBasedXSEventFilterImpl;
-import org.processmining.streambasedeventlog.help.StreamBasedEventLogHelp;
 import org.processmining.streambasedeventlog.parameters.ConditionalProbabilitiesBasedXSEventFilterParametersImpl;
 import org.processmining.streambasedeventlog.parameters.StreamBasedEventLogParametersImpl;
 import org.processmining.streambasedeventlog.parameters.StreamBasedEventStorageParametersImpl;
 
-@Plugin(name = "Spurious Event Filter", parameterLabels = { "Event Stream", "Parameters" }, returnLabels = { "Hub",
-		"Event Stream" }, returnTypes = { XSEventHub.class, XSEventStream.class }, help = StreamBasedEventLogHelp.TEXT)
+//@Plugin(name = "Spurious Event Filter", parameterLabels = { "Event Stream", "Parameters" }, returnLabels = { "Hub",
+//		"Event Stream" }, returnTypes = { XSEventHub.class, XSEventStream.class }, help = StreamBasedEventLogHelp.TEXT)
 @Deprecated // moved to StreamBasedEventFilter package
 public class SpuriousEventFilterPlugin {
 
-	@PluginVariant(variantLabel = "Spurious Event Filter, stream / parameters", requiredParameterLabels = { 0, 1 })
+//	@PluginVariant(variantLabel = "Spurious Event Filter, stream / parameters", requiredParameterLabels = { 0, 1 })
 	public Object[] run(final PluginContext context, final XSEventStream stream,
 			final StreamBasedEventStorageParametersImpl parameters) {
 		//		XSEventHub hub = new TrieBasedSpuriousEventFilterStatisticsImpl(parameters,
@@ -46,20 +42,20 @@ public class SpuriousEventFilterPlugin {
 		return new Object[] { hub, out };
 	}
 
-	@PluginVariant(variantLabel = "Spurious Event Filter, stream", requiredParameterLabels = { 0 })
+//	@PluginVariant(variantLabel = "Spurious Event Filter, stream", requiredParameterLabels = { 0 })
 	public Object[] runDefault(PluginContext context, XSEventStream stream) {
 		StreamBasedEventStorageParametersImpl parameters = new StreamBasedEventStorageParametersImpl();
 		return run(context, stream, parameters);
 	}
 
-	@UITopiaVariant(affiliation = "Eindhoven University of Technology", author = "Sebastiaan J. van Zelst", email = "s.j.v.zelst@tue.nl")
-	@PluginVariant(variantLabel = "Spurious Event Filter, UI", requiredParameterLabels = { 0 })
+//	@UITopiaVariant(affiliation = "Eindhoven University of Technology", author = "Sebastiaan J. van Zelst", email = "s.j.v.zelst@tue.nl")
+//	@PluginVariant(variantLabel = "Spurious Event Filter, UI", requiredParameterLabels = { 0 })
 	public Object[] runUI(final UIPluginContext context, final XSEventStream stream) {
 		return run(context, stream, new StreamBasedEventStorageParametersImpl());
 	}
 
-	@UITopiaVariant(affiliation = "Eindhoven University of Technology", author = "Sebastiaan J. van Zelst", email = "s.j.v.zelst@tue.nl")
-	@PluginVariant(variantLabel = "Spurious Event Filter, UI", requiredParameterLabels = { 0 })
+//	@UITopiaVariant(affiliation = "Eindhoven University of Technology", author = "Sebastiaan J. van Zelst", email = "s.j.v.zelst@tue.nl")
+//	@PluginVariant(variantLabel = "Spurious Event Filter, UI", requiredParameterLabels = { 0 })
 	public Object[] runUI(final UIPluginContext context, final XSEventAuthor author) {
 		List<XSEventStream> availableStreamsOfAuthor = new ArrayList<>();
 		try {
